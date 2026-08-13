0ecd16abe3493df21b59b42abf4596694a9a7882
bbaakr13-cell <bbaakr13@gmail.com>
2026-08-13 11:41:04 +0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,40 @@
+import { Routes, Route } from 'react-router-dom';
+import { DashboardPage } from '@/pages/DashboardPage';
+import { TransactionsPage } from '@/pages/TransactionsPage';
+import { AddPage } from '@/pages/AddPage';
+import { AddEquipmentPage } from '@/pages/AddEquipmentPage';
+import { EditEquipmentPage } from '@/pages/EditEquipmentPage';
+import { CustomersPage } from '@/pages/CustomersPage';
+import { CustomerDetailPage } from '@/pages/CustomerDetailPage';
+import { CustomerDetailPage } from '@/pages/CustomerDetailPage';
+import { EquipmentPage } from '@/pages/EquipmentPage';
+import { EquipmentDetailPage } from '@/pages/EquipmentDetailPage';
+import { MonthlyPage } from '@/pages/MonthlyPage';
+import { MonthlyDetailPage } from '@/pages/MonthlyDetailPage';
+import { ReportsPage } from '@/pages/ReportsPage';
+import { InvoicesPage } from '@/pages/InvoicesPage';
+import { SettingsPage } from '@/pages/SettingsPage';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<DashboardPage />} />
+      <Route path="/transactions" element={<TransactionsPage />} />
+      <Route path="/add" element={<AddPage />} />
+      <Route path="/equipment/add" element={<AddEquipmentPage />} />
+      <Route path="/equipment/:id/edit" element={<EditEquipmentPage />} />
+      <Route path="/customers" element={<CustomersPage />} />
+      <Route path="/customers/:id" element={<CustomerDetailPage />} />
+      <Route path="/customers/:id" element={<CustomerDetailPage />} />
+      <Route path="/equipment" element={<EquipmentPage />} />
+      <Route path="/equipment/:id" element={<EquipmentDetailPage />} />
+      <Route path="/monthly" element={<MonthlyPage />} />
+      <Route path="/monthly/:id" element={<MonthlyDetailPage />} />
+      <Route path="/reports" element={<ReportsPage />} />
+      <Route path="/invoices" element={<InvoicesPage />} />
+      <Route path="/settings" element={<SettingsPage />} />
+    </Routes>
+  );
+}
+
+export default App;
