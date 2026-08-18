903146d6280cadb2b560e2f275a26c6a784f3023
bbaakr13-cell <bbaakr13@gmail.com>
2026-08-18 17:18:04 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import { EquipmentDetailPage } from '@/pages/EquipmentDetailPage';
 import { MonthlyPage } from '@/pages/MonthlyPage';
 import { MonthlyDetailPage } from '@/pages/MonthlyDetailPage';
 import { ReportsPage } from '@/pages/ReportsPage';
-import { InvoicesPage } from '@/pages/InvoicesPage-2';
+import { InvoicesPage } from '@/pages/InvoicesPage-2-new-v2';
 import { SettingsPage } from '@/pages/SettingsPage';
 import { DailyCalculatorPage } from '@/pages/DailyCalculatorPage';
 import { MonthlyRentalPage } from '@/pages/MonthlyRentalPage';
